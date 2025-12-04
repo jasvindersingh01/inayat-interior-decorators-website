ad51334696b3b24e2372b713b87e00a93c0f735c
Jasvinder Singh <jasvindersingh01@gmail.com>
2025-12-04 14:36:07 +0530

    add console for check

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -1,27 +1,24 @@
-const Contact = require("../models/Contact");
-const sendNotificationEmail = require("../utils/mailer");
-
 exports.submitForm = async (req, res) => {
-  console.log("New Request Received");
+  console.log("🔥 New Request Received");
+  console.log("Body:", req.body);
+
   try {
     const { name, phone, email, message } = req.body;
+
+    console.log("🟢 Saving to Database...");
 
     const newLead = new Contact({ name, phone, email, message });
     await newLead.save();
 
+    console.log("🟢 DB Saved. Sending email...");
+
     await sendNotificationEmail(name, phone, email, message);
 
-    res.status(201).json({
-      success: true,
-      message: "Form submitted successfully!",
-      data: newLead,
-    });
+    console.log("🟢 Email sent successfully!");
 
+    res.status(201).json({ success: true });
   } catch (err) {
-    res.status(500).json({
-      success: false,
-      message: "Server Error",
-      error: err.message,
-    });
+    console.log("❌ ERROR:", err.message);
+    res.status(500).json({ success: false, error: err.message });
   }
 };
